ebe4363872e7be4030019d358c0294a4ea050253
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-12 09:35:10 +0800

    Add abstract `Animal` class

--- src/1.cpp
+++ src/1.cpp
@@ -3,11 +3,18 @@
     Please implement these three classes.
 */
 
+class Animal
+{
+    virtual void eat() = 0;
+};
+
 void main()
 {
     Dog dog;
     Cat cat;
-    Animal animal; // i want the compiler to disallow this
+
+    // Animal animal; // i want the compiler to disallow this
+
     Animal *pAnimal = &dog;
     cout << pAnimal->eat(); // expected output is "Eats bone"
     pAnimal = &cat;
