426e06cb8a4b4a75c2d2f039eabcd91d1c6cd42c
Daryll Panaligan <50567021+daryll-panaligan@users.noreply.github.com>
2025-02-12 11:33:53 +0800

    Implement `create` and `destroy` static functions
    - `create` allocates memory of size sizeof(CShell) to static pointer member with type `void *` and returns it
    - destroy deletes the memory allocated at `sp_instance`

--- src/4.cpp
+++ src/4.cpp
@@ -3,30 +3,40 @@
 4. Please implement a class CShell that will be used as follows. What does pShell point to after
 CShell::destroy() is called?
 */
+#include <cstdlib>
 
 class CShell
 {
 private:
     void *operator new(size_t);
-    void *operator new[](size_t);
     void operator delete(void *);
-    void operator delete[](void *);
+
+    static void *sp_instance;
 
 public:
     static CShell *create()
     {
-        static CShell s_instance;
-        return &s_instance;
+        sp_instance = malloc(sizeof(CShell));
+        return (CShell *)sp_instance;
+    };
+
+    static void destroy()
+    {
+        delete sp_instance;
     };
 };
 
-void main()
+void *CShell::sp_instance = nullptr;
+
+int main()
 {
     // CShell *pShell = new CShell;       // should be disallowed by compiler
 
     CShell *pShell = CShell::create(); // should do what above does
 
-    // // do stuff with pShell...
+    // do stuff with pShell...
     // delete pShell; // should be disallowed by compiler
-    // CShell::destroy(); // should deallocate memory allocated in create call
+    CShell::destroy(); // should deallocate memory allocated in create call
+
+    // ANSWER: pShell points to an invalid memory location (since it has been de-allocated in `CShell::destroy`)
 }
